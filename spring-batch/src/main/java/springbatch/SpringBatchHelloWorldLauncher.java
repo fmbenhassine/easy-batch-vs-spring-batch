d7f5466114f87467790a0e41119a28cfc3ee2e6b
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2017-11-19 14:19:57 +0100

    fix typo

--- spring-batch/src/main/java/springbatch/SpringBatchHelloWorldLauncher.java
+++ spring-batch/src/main/java/springbatch/SpringBatchHelloWorldLauncher.java
@@ -7,18 +7,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Spring Batch hellow world launcher.
+ * Spring Batch hello world launcher.
  */
 public class SpringBatchHelloWorldLauncher {
 
     public static void main(String[] args) throws Exception {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("job-hello-world.xml");
-
         JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
-
         Job job = (Job) context.getBean("helloWorldJob");
-
         jobLauncher.run(job, new JobParameters());
 
     }
